3c76fb953dfeaa07263f51b1ca4daab3efb26036
fwanguem <ferwanguer@me.com>
2022-10-10 16:05:29 +0200

    updated test to new structure

--- tests/test_training.py
+++ tests/test_training.py
@@ -3,14 +3,15 @@ import numpy as np
 
 # sys.path.append('c:\\Users\\fwanguem\\Desktop\\PyQEA_package')
 
-import PyQEA
+from PyQEA import QuantumEvAlgorithm
+from PyQEA.utils.cost_functions import f
 
 n_dims = 10
 up = 5*np.ones(n_dims)
 low = -5*np.ones(n_dims)
 integrals = np.full(n_dims, False)
 
-optimizer = PyQEA.QuantumEvAlgorithm(PyQEA.f, n_dims=n_dims, upper_bound=up,
+optimizer = QuantumEvAlgorithm(f, n_dims=n_dims, upper_bound=up,
                                      lower_bound=low, integral_id=integrals,
                                      sigma_scaler=1.003,
                                      mu_scaler=20, elitist_level=6,
