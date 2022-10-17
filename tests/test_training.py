275bf2161064bcf212c4810da112155ed149c0a7
fwanguem <ferwanguer@me.com>
2022-10-17 11:11:32 +0200

    small change in test

--- tests/test_training.py
+++ tests/test_training.py
@@ -6,18 +6,18 @@ import numpy as np
 from PyQEA import QuantumEvAlgorithm
 from PyQEA.utils.cost_functions import f,g, rastrigin
 
-n_dims = 5
+n_dims = 2
 up = 5*np.ones(n_dims)
 low = -5*np.ones(n_dims)
 integrals = np.full(n_dims, False)
 
-optimizer = QuantumEvAlgorithm(f, n_dims=n_dims, upper_bound=up,
+optimizer = QuantumEvAlgorithm(g, n_dims=n_dims, upper_bound=up,
                                      lower_bound=low, integral_id=integrals,
-                                     sigma_scaler=1.001,
-                                     mu_scaler=30, elitist_level=10,
+                                     sigma_scaler=1.03,
+                                     mu_scaler=10, elitist_level=10,
                                      restrictions=[])
 
-results = optimizer.training(N_iterations=27000, sample_size=20, save=False,
+results = optimizer.training(N_iterations=1500, sample_size=20, save=False,
                              filename='q11.npz')
 
 
